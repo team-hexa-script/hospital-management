84ef9db3b38bb2d1d57d5575ec46b885034fafb6
anighosal <121676026+anighosal@users.noreply.github.com>
2023-08-10 20:15:43 +0600

    responsive nav added

--- src/app/(with-layout)/profile/page.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ProfilePage = () => {
-  return <div>profile page</div>;
-};
-
-export default ProfilePage;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import NavLink from "./NavLink";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
@@ -36,12 +36,22 @@ const navLinks = [
 ];
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
   return (
     <div className=" bg-white shadow-md ">
       <nav className="flex items-center justify-between container mx-auto py-2">
         <h1 className="text-3xl font-semibold">hospital</h1>
 
-        <ul className="flex items-center py-8 font-semibold justify-center">
+        {/* Dekstop Menu */}
+        <ul
+          className={`md:flex ${
+            menuOpen ? "block" : "hidden"
+          } md:items-center md:py-8 md:font-semibold md:justify-center`}
+        >
           {navLinks.map(({ path, title }) => (
             <li className="mx-2" key={path}>
               <NavLink
@@ -53,8 +63,33 @@ const Navbar = () => {
               </NavLink>
             </li>
           ))}
+
+          {/* Search Icon */}
           <FontAwesomeIcon icon={faSearch} />
         </ul>
+
+        {/* Mobile Menu Button */}
+        <div className="flex md:hidden">
+          <button
+            onClick={toggleMenu}
+            className="text-gray-500 hover:text-blue-800 focus:outline-none"
+          >
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          </button>
+        </div>
       </nav>
     </div>
   );
