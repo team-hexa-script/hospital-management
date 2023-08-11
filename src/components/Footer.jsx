b0ffc53f9e939ce8c62b539ab0098d1a5edbd5c7
BidhanCR <bidhanroy300805@gmail.com>
2023-08-11 15:48:33 +0600

    footer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+"use client"
+
+import React from 'react';
 
 const Footer = () => {
   return (
-    <div>This our Footer</div>
-  )
-}
+    <footer className="bg-blue-900 text-white">
+      <div className="container mx-auto py-8 flex flex-col md:flex-row justify-between items-center">
+        <div className="text-center md:text-left mb-4 md:mb-0">
+          <h2 className="text-2xl font-semibold">Hospital Management System</h2>
+          <p className="mt-2">Your Address Line 1</p>
+          <p>Your Address Line 2</p>
+        </div>
+        <div className="flex mt-4 md:mt-0">
+          <a
+            href="/"
+            className="mx-2 hover:underline"
+          >
+            Home
+          </a>
+          <a
+            href="/about"
+            className="mx-2 hover:underline"
+          >
+            About Us
+          </a>
+          <a
+            href="/services"
+            className="mx-2 hover:underline"
+          >
+            Services
+          </a>
+          <a
+            href="/contact"
+            className="mx-2 hover:underline"
+          >
+            Contact Us
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
